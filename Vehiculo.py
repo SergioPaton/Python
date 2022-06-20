fea01c4a8f17484bac5ee064649ef7f40465a1ab
SergioPaton <106663134+SergioPaton@users.noreply.github.com>
2022-06-20 14:50:30 +0200

    Add files via upload

--- Vehiculo.py
+++ Vehiculo.py
@@ -16,6 +16,9 @@ class Coche(Vehiculo):
         super().__init__(color, ruedas, puertas)
         self.velocidad=velocidad
         self.cilindrada=cilindrada
+    
+    def __str__(self):
+          return "color {}, {} km/h, {} ruedas, {} puertas, {} cc".format( self.color, self.velocidad, self.ruedas, self.puertas, self.cilindrada )
 
 p = Coche("Rojo", 4, 5, 120, 150)
 print(p)
